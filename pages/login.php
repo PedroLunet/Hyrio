0847e4f6941a003961550f5d6975ddcf4267e929
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-05-09 19:32:35 +0100

    early design of login page

--- pages/login.php
+++ pages/login.php
@@ -5,7 +5,23 @@ declare( strict_types=1 );
 require_once(__DIR__ . '/../includes/common.php');
 
 head();
+
+echo '<link rel="stylesheet" href="/css/auth.css">';
+
 drawHeader();
+?>
+
+<main>
+    <form class="login-form" action="/actions/login.php" method="POST">
+        <h1>Welcome back!</h1>
+        <input class="form-item" type="email" name="email" placeholder="Email" required>
+        <input class="form-item" type="password" name="password" placeholder="Password" required>
+        <input class="form-item" type="submit" value="Login">
+    </form>
+</main>
+
+<?php
+
 drawFooter();
 
 ?>
